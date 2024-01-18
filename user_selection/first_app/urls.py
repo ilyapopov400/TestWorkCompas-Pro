3582c1637d21b73a319c1c85ca623f83927f0910
ilya_popov <il_pop@mail.ru>
2024-01-18 17:19:08 +0300

    форма регистрации выводится, но не сохраняется

--- user_selection/first_app/urls.py
+++ user_selection/first_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.FirstAppIndex.as_view(), name='first-app-index'),  # просмотр главной страницы
+    path('register', views.RegisterUser.as_view(), name='register'),  # регистрация пользователя
     path('family/', views.FamilyShow.as_view(), name='family-show'),  # просмотр всех записей в таблице
     path('api/v1/familylist/', views.FamilyAPIViews.as_view()),  # просмотр с api
 ]
--- user_selection/first_app/views.py
+++ user_selection/first_app/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
-
 from rest_framework import generics
 
 from django.views.generic.base import TemplateView
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.views.generic.edit import FormView
+from rest_framework.reverse import reverse_lazy
 
 from . import models
 from . import serializers
+from . import forms
 
 
 # Create your views here.
@@ -28,5 +29,25 @@ class FamilyShow(ListView):
 
 
 class FamilyAPIViews(generics.ListAPIView):
+    '''
+    просмотр всех записей в таблице Famil API
+    '''
     queryset = models.Family.objects.all()
     serializer_class = serializers.FamilySerializer
+
+
+class RegisterUser(FormView, CreateView):
+    '''
+    регистрация пользователя
+    '''
+    form_class = forms.FormRegisterUser
+    template_name = "first_app/register_user.html"
+    success_url = reverse_lazy('first-app-index')
+
+    def form_valid(self, form):
+        form.save()
+        return super(RegisterUser, self).form_valid(form)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return dict(list(context.items()))
