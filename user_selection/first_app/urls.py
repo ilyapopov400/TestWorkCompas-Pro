26dd767cf3ce5e0c6fe0898a9a1f7376e1208e4d
ilya_popov <il_pop@mail.ru>
2024-01-18 17:50:40 +0300

    добавлена аутентификация по api

--- user_selection/first_app/urls.py
+++ user_selection/first_app/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -6,4 +6,5 @@ urlpatterns = [
     path('register', views.RegisterUser.as_view(), name='register'),  # регистрация пользователя
     path('family/', views.FamilyShow.as_view(), name='family-show'),  # просмотр всех записей в таблице
     path('api/v1/familylist/', views.FamilyAPIViews.as_view()),  # просмотр с api
+    path('api/v1/register/', include('rest_framework.urls')),  # аутентификация api
 ]
