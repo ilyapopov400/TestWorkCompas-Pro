aee11dfcf1d8ca52f6baaec85b27cd626d1117d5
ilya_popov <il_pop@mail.ru>
2024-01-23 10:31:20 +0300

    login_user, logout_user - аутентификация пользователя

--- user_selection/users/urls.py
+++ user_selection/users/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
+    path('register/', views.RegisterIndex.as_view(), name='register'),  # просмотр главной страницы регистрации
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
 ]
--- user_selection/users/views.py
+++ user_selection/users/views.py
@@ -1,12 +1,52 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.views.generic.base import TemplateView
+
+from . import forms
 
 
 # Create your views here.
 
+class RegisterIndex(TemplateView):
+    '''
+    просмотр главной страницы регистрации
+    '''
+    template_name = 'users/index.html'
+
+
 def login_user(request):
-    return HttpResponse("login")
+    '''
+    аутенфикация пользователя
+    :param request:
+    :return:
+    '''
+
+    if request.method == "POST":
+        form = forms.LoginUserForm(request.POST)
+        if form.is_valid():
+            cl_data = form.cleaned_data
+            user = authenticate(request,
+                                username=cl_data["username"],
+                                password=cl_data["password"])
+            print(user, cl_data["username"], cl_data["password"])  # TODO
+            if user and user.is_active:
+                login(request=request, user=user)
+                return HttpResponseRedirect(reverse("family-show"))
+    else:
+        form = forms.LoginUserForm()  # для метода GET and ather
+
+    template_name = "users/login.html"
+    date = {"form": form}
+    return render(request=request, template_name=template_name, context=date)
 
 
 def logout_user(request):
-    return HttpResponse("logout")
+    '''
+    отключение пользователя
+    :param request:
+    :return:
+    '''
+    logout(request=request)
+    return HttpResponseRedirect(reverse("users:login"))
